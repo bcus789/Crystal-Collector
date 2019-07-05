ea5245ae2827e80ed86e094e7fc0b334f511ce39
Charlie NoFace <youremail@gmail.comh.e.ergaster@gmail.comgit config --global user.email youremail@gmail.com>
2019-07-05 14:52:39 -0700

    updated ui and made fully responsive

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  var wins = 0;
+  var losses = 0;
+  var userNum = 0;
+  var compNum = randomNum();
+  var crystals;
+
+  function randomNum() {
+    return Math.floor(Math.random() * 102) + 19;
+  }
+console.log(compNum)
+})
