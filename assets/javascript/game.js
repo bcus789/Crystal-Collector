2e88da7af302fc2bd186226e4cd27e8944c64926
Charlie NoFace <youremail@gmail.comh.e.ergaster@gmail.comgit config --global user.email youremail@gmail.com>
2019-07-09 21:51:31 -0700

    just need to get new game state working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,6 +20,40 @@ function randomNum() {
   return Math.floor(Math.random() * 102) + 19;
 }
 
+$("#beep").on ('click', function(e){
+  console.log(userNum)
+  userNum = userNum + yellowBam
+  $('#userNum').text(userNum);
+  winState()
+})
+
+$("#boop").on ('click', function(e){
+  userNum = userNum + superIce
+  $('#userNum').text(userNum);
+  winState()
+})
+
+$("#baap").on ('click', function(e){
+  userNum = userNum + laGlass
+  $('#userNum').text(userNum);
+  winState()
+})
+
+  
+  function winState(){
+  if (userNum === compNum) {
+    wins += 1
+    console.log('win')
+    newGame();
+   }
+  else if (userNum > compNum) {
+    losses++;
+    console.log('loss')
+    newGame();
+  }
+  }
+
+   
 console.log(compNum)
 $("#compNum").append($("<p>").text(compNum));
 $("#wins").append($("<p>").text(wins));
@@ -33,26 +67,7 @@ let yellowBam = Math.floor(Math.random() * 12) + 1
 let superIce = Math.floor(Math.random() * 12) + 1
 let laGlass = Math.floor(Math.random() * 12) + 1
 
-$("#beep").on ('click', function(e){
-  userNum = userNum + yellowBam
-  $('#userNum').text(userTotal);})
-
-$("#boop").on ('click', function(e){
-  userNum = userNum + yellowBam
-  $('#userNum').text(userTotal);})
-
-$("#baap").on ('click', function(e){
-  userNum = userNum + yellowBam
-  $('#userNum').text(userTotal);})
-
    // win/loss state
- if (userNum === compNum) {
-  wins++;
-  newGame();
- }
- else if (userNum > compNum) {
-  losses++;
-  newGame();
- }
- 
+   
+
  
